dc30cd5442b90ccc62df52548dff5cf4e29ec778
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-27 18:21:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,21 +34,6 @@ end
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
-end
-
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
